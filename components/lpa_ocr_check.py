928f90612acbd5a03a5d68e4c44dd6559490cd5a
Zhang Yi Jiang <mediumdeviation@gmail.com>
2018-09-13 19:27:51 +0800

    Remove unused components

--- components/lpa_ocr_check.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-from meya import Component
-
-# Checks that all fields have been filled by the OCR
-class LpaOcrCheck(Component):
-    def start(self):
-        
-        message = self.create_message(text=text)
-        return self.respond(message=message, action="next")
